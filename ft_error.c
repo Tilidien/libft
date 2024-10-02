6cf9b75866e19c61440a573009ad241b529735cf
Tilman Gmelin <Tilmangmelin@gmx.de>
2024-10-02 12:27:55 +0200

    moved ft_error to libft

---
+++ ft_error.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_error.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tgmelin < tgmelin@student.42heilbronn.d    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/02 12:23:57 by tgmelin           #+#    #+#             */
+/*   Updated: 2024/10/02 12:23:57 by tgmelin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_error(t_ft_err_data *_data, const char *_msg, int _rtrn)
+{
+	static void	*allocated_data;
+	static void	(*err_function)(void *_allocated_data);
+
+	if (_data)
+	{
+		allocated_data = _data->alloc_data;
+		err_function = _data->teardown_func;
+		return ;
+	}
+	write(2, _msg, ft_strlen(_msg));
+	err_function(allocated_data);
+	exit(1);
+}
