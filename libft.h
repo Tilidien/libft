c99188ef7df79231943ee6f681dad1801568966c
Tilman Gmelin <tgmelin@1-E-11.42heilbronn.de>
2024-10-17 15:22:47 +0200

    add get next line to libft

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tgmelin <tgmelin@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 14:50:22 by tgmelin           #+#    #+#             */
-/*   Updated: 2024/10/11 01:30:38 by tgmelin          ###   ########.fr       */
+/*   Updated: 2024/10/17 15:19:35 by tgmelin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,32 @@
 # include <unistd.h>
 # include <stdarg.h> //va_start, va_end, va_arg, va_list
 # include <stdbool.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif //BUFFER_SIZE
 
 typedef char	t_bool;
 
 # define TRUE 1
 # define FALSE 0
+
+# define MAX_FILE_DESCRIPTORS 1024
+
+typedef struct s_gnl_buffer
+{
+	char	buffer[MAX_FILE_DESCRIPTORS][BUFFER_SIZE];
+	ssize_t	len[MAX_FILE_DESCRIPTORS];
+}	t_gnl_buffer;
+
+typedef struct s_gnl_vars
+{
+	char	*out;
+	ssize_t	out_size;
+	ssize_t	endl;
+	ssize_t	bytes_read;
+	ssize_t	bytes_newly_read;
+	char	init;
+}	t_gnl_vars;
 
 typedef struct s_list
 {
@@ -120,5 +141,7 @@ void	ft_error(t_ft_err_data *_data, const char *_msg, int _rtrn );
 t_list	*ft_slstnew(void *_content);
 t_list	*ft_lstpop(t_list **_lst);
 int		ft_lstcount(t_list **_lst);
+void	ft_memclr(char *_mem, size_t _size);
+char	*get_next_line(int fd);
 
 #endif
