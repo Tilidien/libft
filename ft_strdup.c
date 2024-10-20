09c0f84a7800f743bb531970a0ab34362b992ce7
Tilman Gmelin <tgmelin@1-E-7.42heilbronn.de>
2024-10-20 23:18:42 +0200

    why did i ever let it use ft_error

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: tgmelin <tgmelin@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 20:49:35 by tgmelin           #+#    #+#             */
-/*   Updated: 2024/10/09 23:57:33 by tgmelin          ###   ########.fr       */
+/*   Updated: 2024/10/20 23:18:18 by tgmelin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,7 @@ char	*ft_strdup(const char *_src)
 	len = ft_strlen(_src);
 	out = (char *)malloc(len + 1);
 	if (!out)
-	{
-		return (ft_error(NULL, "MALLOC ERROR", EXIT_SUCCESS), NULL);
-	}
+		return (NULL);
 	i = -1;
 	while (_src[++i])
 	{
